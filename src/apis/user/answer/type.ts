98b37ed4aff06039feda3d3224474f14f3d05152
knh6269 <knh6269@gmail.com>
2023-05-02 18:20:35 +0900

    refactor: change file name (#37)

---
+++ src/apis/user/answer/type.ts
@@ -0,0 +1,16 @@
+type answer = {
+  number: number;
+  answer: string;
+};
+
+type parentType = "FATHER" | "MOTHER";
+
+type RequestData = {
+  parentType: parentType;
+  answers: answer[];
+};
+
+// POST /api/v1/child/answer
+
+// 채점
+// POST /api/v1/parent/answer

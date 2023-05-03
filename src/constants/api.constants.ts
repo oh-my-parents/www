cf8e224a7b4f6be43b744bff50fac8869293efc2
nerdchanii <nerdchanii@gmail.com>
2023-05-03 16:34:41 +0900

    build(script): implement ci/cd pipeline(#14)(#49)
    * chore(ignore): update ignore for env(#14)
    * refactor(constants): update constants(#14)
    * feat(env): add env template for deploy(#14)
    * feat(scripts): add deploy scripts(#14)

--- src/constants/api.constants.ts
+++ src/constants/api.constants.ts
@@ -1,15 +1,16 @@
-const HOST = import.meta.env.VITE_APP_HOST;
+const APP_HOST = import.meta.env.VITE_APP_HOST;
+const API_HOST = import.meta.env.VITE_APP_API_HOST;
 const PREFIX = "api";
 const VERSION = "v1";
 
-export const BASE_URL = `${HOST}/${PREFIX}/${VERSION}`;
+export const BASE_URL = `${API_HOST}/${PREFIX}/${VERSION}`;
 export const DEFAULT_TIME_OUT = 10_000;
 export const AUTH_REQUIRED_REGEX_LIST: RegExp[] = [/users/];
 
 export const KAKAO = Object.freeze({
   OAUTH_HOST: "https://kauth.kakao.com",
   REST_API_KEY: import.meta.env.VITE_APP_KAKAO_API_KEY,
-  REDIRECT_URI: `${HOST}/oauth`,
+  REDIRECT_URI: `${APP_HOST}/oauth`,
   GRANT_TYPE: "authorization_code",
 });
 

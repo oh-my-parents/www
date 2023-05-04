c3980713df773edde07899f1ed9ccbcc5182b605
knh6269 <knh6269@gmail.com>
2023-05-04 22:23:22 +0900

    feat: develop drop down component(#66)

---
+++ src/components/ui/molecules/DropDown/Modal.tsx
@@ -0,0 +1,25 @@
+import $ from "./index.module.scss";
+
+type Props = {
+  array: number[] | string[];
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
+  setClick: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export default function Modal({ setState, setClick, array }: Props) {
+  const onClickModal = (e: any) => {
+    setState(false);
+    setClick(e);
+  };
+  return (
+    <div className={$.dropDownModalDiv}>
+      {array.map((item) => {
+        return (
+          <div onClick={() => onClickModal(item)} className={$.dropDownModal}>
+            <strong>{item}</strong>
+          </div>
+        );
+      })}
+    </div>
+  );
+}

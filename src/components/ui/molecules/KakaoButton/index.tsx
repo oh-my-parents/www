a6c6c17b0790fed452c654bb07022ceed193aae6
knh6269 <knh6269@gmail.com>
2023-05-06 03:44:04 +0900

    feat: develop share page (#111)

--- src/components/ui/molecules/KakaoButton/index.tsx
+++ src/components/ui/molecules/KakaoButton/index.tsx
@@ -1,21 +1,24 @@
 import Button from "../../atoms/Button/Button";
 import IconTypography from "../IconTypography";
 import kakaoTalk from "/images/kakaotalk.svg";
-import { KAKAO_BUTTON_URL } from "@/constants/api.constants";
 
-export default function KakaoButton() {
-  const onClickKakaoButton = () => {
-    window.location.replace(KAKAO_BUTTON_URL);
-  };
+type Props = {
+  children: string;
+  onClick: any;
+  className: string;
+};
 
+export default function KakaoButton({ children, onClick, className }: Props) {
   return (
-    <Button size="medium" variant="kakao" onClick={onClickKakaoButton}>
-      <IconTypography
-        src={kakaoTalk}
-        size="small"
-        variant="kakao"
-        children="카카오톡으로 시작하기"
-      />
+    <Button
+      size="medium"
+      variant="kakao"
+      onClick={onClick}
+      className={className}
+    >
+      <IconTypography src={kakaoTalk} size="small" variant="kakao">
+        {children}
+      </IconTypography>
     </Button>
   );
 }

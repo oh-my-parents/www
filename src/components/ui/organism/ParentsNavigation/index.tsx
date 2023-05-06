35d098a02838686dee5dc8f93bc6c957ade0ed47
nerdchanii <nerdchanii@gmail.com>
2023-05-07 07:00:44 +0900

    feat(add): parents navigation(#128)

---
+++ src/components/ui/organism/ParentsNavigation/index.tsx
@@ -0,0 +1,31 @@
+import arrowLeft from "/images/arrowLeft.svg";
+import IconButton from "../../molecules/IconButton";
+import Logo from "../../molecules/Logo";
+import $ from "./index.module.scss";
+import cn from "classnames";
+
+type Props = {};
+
+const index = (props: Props) => {
+  const onClickLeft = () => {
+    console.log("onClickLeft");
+  };
+
+  return (
+    <div className={cn($.navigation)}>
+      <IconButton
+        src={arrowLeft}
+        onClick={onClickLeft}
+        iconSize="small"
+        iconVariant="navigation"
+        buttonVariant="navigation"
+        buttonSize="small"
+      />
+      <div className={$.logoContainer}>
+        <Logo size="medium" variant="gradient" />
+      </div>
+    </div>
+  );
+};
+
+export default index;

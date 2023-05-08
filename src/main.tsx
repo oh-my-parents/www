9929895a409a901a429608727bcba62345749433
nerdchanii <nerdchanii@gmail.com>
2023-05-08 15:32:00 +0900

    fix(layout): change height vh to svh

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Sample from "./routes/Sammple";
 import Callback from "./routes/Callback";
 import Main from "./routes/main";
 import { RecoilRoot } from "recoil";
